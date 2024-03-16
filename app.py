4395f34ead5556ec3ff0c696d370b24a2e28a7fa
Ulrich Segodo <segodoulrich21@gmail.com>
2024-03-16 11:51:29 +0100

    add models requirement and app.py update and cheat sheet

--- app.py
+++ app.py
@@ -1,51 +1,65 @@
-import streamlit as st
+import pathlib
 import random
+
+import streamlit as st
+import torch
+
 from PIL import Image
+from fastai.vision.all import *
+
+# fastai load_learner uses PosixPath, use a trick to load the model
+_saved_hack = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+classification_model = load_learner('models/palmoil_fastai_resnet18.pth')
+pathlib.PosixPath = _saved_hack
+
 
 def classify_image(image) -> bool:
     """Classify if an image has palm trees or not
-
+    
     Params:
-    -image (bytes)
+    - image (bytes): image to be classify
+    
     Returns:
-    -bool: True for palm tree, False otherwise
+    - bool: True if image contains palm trees
     """
-    return random.choice([True, False])
+    tensor = torch.tensor(np.asarray(Image.open(image).convert('RGB').resize((256, 256))))
+    results = classification_model.predict(tensor)
+    return results[0] == '1'
+    # return random.choice([True, False])
+    
 
 def analyze_image(image):
     """Analyze parts of image that most contributed to classification
-
+    
     Params:
-    -image(bytes): image to analyze
-
+    - image (bytes): image to analyze
+    
     Returns:
-    -bytes: image showing most contributive part of the image
+    - bytes: image showing most contributive part of the image
     """
+    return Image.open(image).convert("L")
+    
 
-    return Image.open(image).convert("L")  # convert image to grayscale
-
-
-st.write("# Palm Tree Detector")
+st.write("## Palm Tree Dectector")
 
 with st.sidebar:
-    image_file = st.file_uploader("Choose an image", 
-                                type=["jpg", "png"],
-                                help="Select image to be classified",
-                                )
+    image_file = st.file_uploader('Choose an image',
+                                  type=['png', 'jpg'],
+                                  help='Select image to be classified',
+                                 )
 
 if image_file is not None:
     bytes_image = image_file.getvalue()
-    st.image(bytes_image, width = 256)
+    st.image(bytes_image, width=256)
 
-if st.button("Detect palm trees", disabled=(image_file is None)):
-    result=classify_image(bytes_image)
+if st.button('Detect palm trees', disabled=(image_file is None)):
+    result = classify_image(image_file)
     if result:
-        st.write("Palm Trees detected")
+        st.write('Palm trees detected')
     else:
-        st.write("No palm Trees detected")
+        st.write('Palm trees not detected')
 
-    with  st.expander("Sensibility Analysis", expanded=False):
+    with st.expander('Sensitivity Analysis', expanded=False):
         analysis_image = analyze_image(image_file)
-        st.image(analysis_image)
-
-# Finished
+        st.image(analysis_image, width=256)
